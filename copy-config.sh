bab2c8986e72c1823152705d64af055abe713f15
kbux <kevin@splinterlands.com>
2022-03-25 15:31:55 -0400

    udpate copy

--- copy-config.sh
+++ copy-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -r ~/accounts.txt ./bot/linux-x64/config/
-cp -r ~/sl-bot-api/bot/config.txt ./bot/linux-x64/config/
+cp -r ../accounts.txt ./bot/linux-x64/config/
+cp -r ../sl-bot-api/bot/config.txt ./bot/linux-x64/config/
